ffaa5fe231d0167ebc74df7cfc18bf5c265c95e3
Brit Butler <redline6561@gmail.com>
2015-01-07 12:22:53 -0500

    Scaffold out the rest of hangman.

--- 01-07/hangman.rb
+++ 01-07/hangman.rb
@@ -15,25 +15,35 @@ words = ['angus',
          'marxist',
          'darwinism']
 
-def take_turn
-  ## TODO: Write me!
-  # decrement turn if letter not in answer
-  # get user input / show some status
-  # add input to set of guessed letters
-  puts 'we are taking a turn'
-end
-
 def finished? (turns, guesses, answer)
   turns.zero? || answer.chars.all? { |l| guesses.include?(l) }
 end
 
-def main(words)
-  turn_count = ARGV.empty? ? 6 : ARGV[0].to_i
-  guessed = Set.new
-  answer = words.sample(1)[0]
-  until finished?(turn_count, guessed, answer)
-    take_turn
-  end
+def greeting
+  # TODO: tell the player about hangman
 end
 
-main(words)
+def game_over
+  # TODO: tell the player what the word was and if they won or lost
+end
+
+def prompt_player
+  # TODO: get user input / show some status
+end
+
+def hangman(words)
+  turn_count = argv.empty? ? 6 : argv[0].to_i
+  guessed = set.new
+  answer = words.sample(1)[0]
+  greeting # TODO: Do I need an argument?
+  until finished?(turn_count, guessed, answer)
+    guess = prompt_player
+    guesses.add(guess)
+    unless answer.include?(guess)
+      turn_count -= 1
+    end
+  end
+  game_over # TODO: Do I need an argument?
+end
+
+hangman(words)
